ef25a9978b9bbd14a51ed9cbc6a7bcdd45eaf8c1
peter renshaw <peterrenshaw@seldomlogical.com>
2016-05-25 19:32:30 +1000

    2016MAY26 more, not running

--- http.elm
+++ http.elm
@@ -3,7 +3,7 @@ import Html.App exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 import Http
-import Json.Decode as json
+import Json.Decode as Json
 import Task
 
 
@@ -15,18 +15,28 @@ import Task
 -- #=======
 
 
+-- MAIN
+main = main.program
+    {
+       init = init "cats"
+     , view = view
+     , update = update
+     , subscriptions = subscriptions
+    }
 
 -- MODEL
 type alias Model = 
     { topic : String
-    , gifUrl : String
+     , gifUrl : String
     }
 
 
 -- INIT 
-init : (Model, Cmd Msg)
+init : String -> (Model, Cmd Msg)
 init = 
-    (Model "cats", "waiting.gif", Cmd.none)
+    ( Model topic "waiting.gif"
+    , getRandomGif topic
+    )
 
 
 -- UPDATE
@@ -34,27 +44,33 @@ type Msg = MorePlease
          | FetchSucceed String
          | FetchFail Http.Error
 
-update : Msg -> Model -> (Model, Cmd.none)
-update msg model = 
-    case msg of
+update : Msg -> Model -> (Model, Cmd Msg)
+update action model = 
+    case action of
         MorePlease ->
             (model, Cmd.none)
 
         FetchSucceed newUrl ->
-            (Model, model.topic newUrl, Cmd.none)
+            (Model  model.topic newUrl, Cmd.none)
 
         FetchFail _ ->
             (model, Cmd.none)
 
 
 -- VIEW
-view Model -> Html Msg
+view : Model -> Html Msg
 view model = 
     div []
         [ h2 [] [text model.topic]
         , img [src model.gifUrl] []
         , button [onClick MorePlease] [text "more please"]
         ]
+
+
+-- SUBCRIPTIONS
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  Sub.none
 
 -- FUNCS
 getRandomGif : String -> Cmd Msg
@@ -63,9 +79,10 @@ getRandomGif topic =
         url = 
             "http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=" ++ topic
     in 
-        Task.perform FetchFail FethSucceed (Http.get decodeGifUrl url)
+        Task.perform FetchFail FetchSucceed (Http.get decodeGifUrl url)
 
 
 decodeGifUrl : Json.Decoder String
 decodeGifUrl = 
     Json.at ["data", "image_url"] Json.string
+
