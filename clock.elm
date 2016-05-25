698f7045ab5de529fd2a228468c76711e226b20c
peter renshaw <peterrenshaw@seldomlogical.com>
2016-05-25 21:22:40 +1000

    2016MAY25 working on boilerplate

--- clock.elm
+++ clock.elm
@@ -1,7 +1,8 @@
 import Html exposing (..)
-import Html.App as Html
-import Html.Attributes exposing (..)
-import Html.Events exposing (..)
+import Html.App as App
+import Svg exposing (..)
+import Svg.Attributes exposing (..)
+import Time exposing (Time, second)
 
 
 -- #======
@@ -13,16 +14,32 @@ import Html.Events exposing (..)
 
 
 -- MAIN
-
+main = 
+    { init = init
+    , view = view
+    , update = update
+    , subscriptions = subscriptions
+    }
 
 
 -- MODEL
+type alias Model = Time
 
 
 -- INIT 
+init : (Model, Cmd Msg)
+init = 
+    (0, Cmd.none)
 
 
 -- UPDATE
+type Msg = 
+    Tick Time
+update : Msg -> Model -> (Model, Cmd Msg)
+update action model =
+    case action of 
+        Tick newTime ->
+            (newTime, Cmd.none)
 
 
 -- VIEW
