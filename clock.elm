d8585b342a31343eb9adff5a7b97e542871a47b8
peter renshaw <peterrenshaw@seldomlogical.com>
2016-05-25 21:17:18 +1000

    2016MAY25 checkin

---
+++ clock.elm
@@ -0,0 +1,36 @@
+import Html exposing (..)
+import Html.App as Html
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+
+
+-- #======
+-- # name: clock.elm
+-- # date: 2016MAY24
+-- # desc: example using SVG 
+-- # src : <http://guide.elm-lang.org/architecture/effects/time.html>
+-- #=======
+
+
+-- MAIN
+
+
+
+-- MODEL
+
+
+-- INIT 
+
+
+-- UPDATE
+
+
+-- VIEW
+
+
+-- SUBCRIPTIONS
+
+
+-- FUNCS
+
+
