6f12f2a4dcd4547bcc705240ac2d7b6c4d979bd5
peter renshaw <peterrenshaw@seldomlogical.com>
2016-06-02 13:36:33 +1000

    2016MAY02 added chat

--- chat.elm
+++ chat.elm
@@ -14,4 +14,68 @@ import Websocket
 
 
 
+-- MAIN
+main = 
+    Html.program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
+
+-- MODEL
+type alias Model = 
+    { input : String
+    , messages : List String
+    }
+
+
+-- INIT
+init : (Model, Cmd Msg)
+init = 
+    (Model "" [], Cmd.none)
+
+
+-- UPDATE
+type Msg 
+    = Input String
+    | Send
+    | NewMessage String
+
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg {input, messags} = 
+    case msg of 
+        Input newInput ->
+            (Model newInput messages, Cmd.none)
+
+        Send ->
+            (Model "" messages, Websocket.send "ws://echo.websocket.org" input)
+
+        NewMessage str ->
+            (Model input (str :: messages), Cmd.none)
+
+
+-- SUBSCRIPTIONS
+subscriptions : Model -> Sub Msg
+subscriptions model = 
+    Websocket.listen "ws://echo.websocket.org" NewMessage
+
+
+-- VIEW
+view : Model -> Html Msg
+view model = 
+    div []
+        [ div [] (List.map viewMessage model.messages)
+        , input [onInput Input] []
+        , button [onClick Send] [text "Send"]
+        ]
+
+
+-- FUNCS
+viewMessage : String -> Html msg
+viewMessage msg = 
+    div [] [text msg]
+
 
