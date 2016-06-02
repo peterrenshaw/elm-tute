33b023391992be2568743ae5d85af8cacbcb74df
peter renshaw <peterrenshaw@seldomlogical.com>
2016-06-02 13:16:34 +1000

    2016MAY02 working on chat

---
+++ chat.elm
@@ -0,0 +1,17 @@
+import Html exposing (..)
+import Html.App as Html
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import Websocket
+
+
+-- #======
+-- # name: chat.elm
+-- # date: 2016JUN02
+-- # desc: example using websockets
+-- # src : <http://guide.elm-lang.org/architecture/effects/web_sockets.html>
+-- #=======
+
+
+
+
